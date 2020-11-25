c98fe4a91876260dd664b8264149054bff4ac424
Dev-Steven <stevenchung650@gmail.com>
2020-11-24 23:22:13 -0800

    created navbar and welcome message on dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import NavBar from './components/navBar';
+import Dashboard from './components/Dashboard';
+
+const App = () => {
+	return (
+		<div>
+			<NavBar />
+			<Dashboard />
+		</div>
+	);
+};
 
 export default App;
