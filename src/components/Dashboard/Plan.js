da4a7e6d1cbd225c45fe5da0188b663efe6ddc0b
Dev-Steven <stevenchung650@gmail.com>
2020-11-26 23:23:41 -0800

    plan section

---
+++ src/components/Dashboard/Plan.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Plan.scss';
+
+const Plan = () => {
+	return (
+		<div className='plan-container'>
+			<h6>Planned Workouts</h6>
+			<ul>
+				<li>Run</li>
+				<li>Jump Rope</li>
+				<li>Chest Day</li>
+				<li>Back Day</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Plan;
