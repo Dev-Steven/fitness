621ceb686a3b2d854217bbe671114af9b68feed5
Dev-Steven <stevenchung650@gmail.com>
2020-11-30 09:59:50 -0800

    created links and addworkout component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
 import NavBar from './components/Navbar/Navbar';
 import Dashboard from './components/Dashboard/Dashboard';
+import AddWorkout from './components/AddWorkout/AddWorkout';
 
 const App = () => {
 	return (
 		<div>
-			<NavBar />
-			<Dashboard />
+			<Router>
+				<NavBar />
+				<Switch>
+					<Dashboard path='/' exact />
+					<AddWorkout path='/addworkout' exact />
+				</Switch>
+			</Router>
 		</div>
 	);
 };
