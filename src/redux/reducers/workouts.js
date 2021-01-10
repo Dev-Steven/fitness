5a86170d63aa4509eeeff11133aaec01e0da19a2
Dev-Steven <stevenchung650@gmail.com>
2021-01-09 21:06:09 -0800

    changes

---
+++ src/redux/reducers/workouts.js
@@ -0,0 +1,143 @@
+const initialState = {
+	workouts: [
+		{
+			name: 'HIIT',
+			calories: '320',
+			duration: '30',
+		},
+		{
+			name: 'Run',
+			calories: '35',
+			duration: '400',
+		},
+	],
+	totals: {
+		calories: 355,
+		standHours: 0,
+		exerciseMinutes: 430,
+	},
+	chart: {
+		labels: ['HIIT', 'Run'],
+		datasets: [
+			{
+				label: 'Workouts',
+				backgroundColor: [
+					'#B21F00',
+					'#C9DE00',
+					'#2FDE00',
+					'#00A6B4',
+					'#6800B4',
+				],
+				hoverBackgroundColor: [
+					'#501800',
+					'#4B5000',
+					'#175000',
+					'#003350',
+					'#35014F',
+				],
+				data: [1, 1],
+			},
+		],
+	},
+};
+
+function newWorkout(state, action) {
+	const updatedLabels = state.chart.labels.concat(action.payload.name);
+	const updatedData = state.chart.datasets[0].data.concat(1);
+	const newState = {
+		...state,
+		workouts: newWorkouts,
+		totals: {
+			...state.totals,
+			calories: state.totals.calories + parseInt(action.payload.calories),
+			exerciseMinutes:
+				state.totals.exerciseMinutes +
+				parseInt(action.payload.duration),
+		},
+		chart: {
+			...state.chart,
+			labels: updatedLabels,
+			datasets: [
+				{
+					...state.chart.datasets[0],
+					data: updatedData,
+				},
+			],
+		},
+	};
+	return newState;
+}
+
+export default function workoutReducer(state = initialState, action) {
+	switch (action.type) {
+		case 'ADD_WORKOUT': {
+			console.log('payload: ', action.payload);
+			const newWorkouts = state.workouts.concat(action.payload);
+			const labelIndex = state.chart.labels.findIndex(
+				label => label === action.payload.name
+			);
+			if (labelIndex >= 0) {
+				// workout exists
+				const updatedData = [...state.chart.datasets[0].data];
+				updatedData[labelIndex] = updatedData[labelIndex] + 1;
+				const newState = {
+					...state,
+					workouts: newWorkouts,
+					totals: {
+						...state.totals,
+						calories:
+							state.totals.calories +
+							parseInt(action.payload.calories),
+						exerciseMinutes:
+							state.totals.exerciseMinutes +
+							parseInt(action.payload.duration),
+					},
+					chart: {
+						...state.chart,
+						datasets: [
+							{
+								...state.chart.datasets[0],
+								data: updatedData,
+							},
+						],
+					},
+				};
+				console.log('workout exists: ', newState);
+				return newState;
+			} else {
+				// new workout
+				// const updatedLabels = state.chart.labels.concat(
+				// 	action.payload.name
+				// );
+				// const updatedData = state.chart.datasets[0].data.concat(1);
+				// const newState = {
+				// 	...state,
+				// 	workouts: newWorkouts,
+				// 	totals: {
+				// 		...state.totals,
+				// 		calories:
+				// 			state.totals.calories +
+				// 			parseInt(action.payload.calories),
+				// 		exerciseMinutes:
+				// 			state.totals.exerciseMinutes +
+				// 			parseInt(action.payload.duration),
+				// 	},
+				// 	chart: {
+				// 		...state.chart,
+				// 		labels: updatedLabels,
+				// 		datasets: [
+				// 			{
+				// 				...state.chart.datasets[0],
+				// 				data: updatedData,
+				// 			},
+				// 		],
+				// 	},
+				// };
+				// console.log('new workout: ', newState);
+				return newWorkout(state, action);
+			}
+		}
+		default:
+			return state;
+	}
+}
